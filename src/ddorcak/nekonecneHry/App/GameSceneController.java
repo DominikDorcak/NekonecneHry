1549e91d651bfee3aed61ea5c97c69dcdb53e78d
dominik <dominik.dorcak@gmail.com>
2018-10-25 11:44:41 +0200

    zmena z ciary na ciarovy graf

--- src/ddorcak/nekonecneHry/App/GameSceneController.java
+++ src/ddorcak/nekonecneHry/App/GameSceneController.java
@@ -2,11 +2,16 @@ package ddorcak.nekonecneHry.App;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.scene.chart.XYChart;
+import ddorcak.nekonecneHry.charts.IntervalAxis;
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
 import javafx.scene.control.Button;
-import javafx.scene.shape.Line;
 
 public class GameSceneController {
+
+    private  IntervalAxis xAxis = new IntervalAxis();
+    private  IntervalAxis yAxis =  new IntervalAxis();
 
     @FXML
     private ResourceBundle resources;
@@ -15,15 +20,30 @@ public class GameSceneController {
     private URL location;
 
     @FXML
-    private Line BigIntervalLine;
+    private Button NextTurnButton;
+
+
 
     @FXML
-    private Button NextTurnButton;
+    private LineChart<?,?> intervalsChart;
 
     @FXML
     void initialize() {
-        assert BigIntervalLine != null : "fx:id=\"BigIntervalLine\" was not injected: check your FXML file 'Untitled'.";
-        assert NextTurnButton != null : "fx:id=\"NextTurnButton\" was not injected: check your FXML file 'Untitled'.";
+        assert NextTurnButton != null : "fx:id=\"NextTurnButton\" was not injected: check your FXML file 'GameScene.fxml'.";
+        //assert intervalsChart != null : "fx:id=\"intervalsChart\" was not injected: check your FXML file 'GameScene.fxml'.";
 
+      intervalsChart = new LineChart<Float,Float>(xAxis,yAxis);
+//        intervalsChart.getYAxis().setTickLabelsVisible(false);
+//        intervalsChart.getYAxis().setTickMarkVisible(false);
+//        intervalsChart.getYAxis().setOpacity(0);
+
+        XYChart.Series series = new XYChart.Series();
+        series.setName("My portfolio");
+        //populating the series with data
+        series.getData().add(new XYChart.Data(1, 23));
+        series.getData().add(new XYChart.Data(50, 23));
+
+        intervalsChart.getData().add(series);
     }
 }
+
