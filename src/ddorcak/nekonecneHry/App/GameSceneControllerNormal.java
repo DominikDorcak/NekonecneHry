aef73bd6fce1286ba92bcba5681d1ccff7ff7894
dominik <dominik.dorcak@gmail.com>
2018-11-12 21:06:20 +0100

    dokoncena normal obtiaznost

--- src/ddorcak/nekonecneHry/App/GameSceneControllerNormal.java
+++ src/ddorcak/nekonecneHry/App/GameSceneControllerNormal.java
@@ -1,5 +1,40 @@
 package ddorcak.nekonecneHry.App;
 
+import ddorcak.nekonecneHry.core.DoubleInterval;
+import ddorcak.nekonecneHry.core.PlayerOneEasy;
+import ddorcak.nekonecneHry.core.PlayerOneNormal;
+import ddorcak.nekonecneHry.core.ValidatorDouble;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
+import java.util.List;
+
 public class GameSceneControllerNormal extends GameSceneControllerDouble {
+    private List<List<DoubleInterval>> pokrytia;
+
+    @FXML
+    void initialize() {
+        TurnLabel.setText("�ah: " + turnCount);
+        pokrytia = PlayerOneNormal.vygenerujRozdelenia(zakladny);
+        int index0 = (int) (Math.round(Math.random()*(pokrytia.size()-1)));
+        aktualnePokrytie = pokrytia.get(index0);
+        naplnData();
+
+        NextTurnButton.setOnAction(eh -> {
+            if ((!vyber.isEmpty()) && ValidatorDouble.cowered(zakladny, vyber)) {
+                Stage stage = (Stage) NextTurnButton.getScene().getWindow();
+                stage.close();
+                System.out.println("Vyhra!!");
+            }
+
+            TurnLabel.setText("�ah: " + turnCount++);
+            int indexN = (int) (Math.round(Math.random()*(pokrytia.size()-1)));
+            aktualnePokrytie = pokrytia.get(indexN);
+            naplnData();
+        });
+
+
+    }
+
 
 }
